4429c4b318d8f813e19e0d99692409c076cc9a39
Moohammad Samaae <m.samaae@hotmail.com>
2020-01-15 01:30:58 +0100

    Fixed edit and update

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -6,13 +6,14 @@ module.exports = {
 
         db.query(query, (err, result) => {
             if (err) {
-                res.json(err)
+                res.json(err);
             }
             res.render('getHomePage', {
                 restaurants: result
             });
         });
     },
+
     // GET ADD RESTAURANT PAGE
     getAddRestaurantPage: (req, res) => {
         res.render('getAddRestaurantPage');
@@ -28,14 +29,51 @@ module.exports = {
        /*  let query = 'INSERT INTO restaurants (restaurant_name, restaurant_city, restaurant_address, restaurant_rating)' +
         'VALUES' ('" + restaurant_name + "', '" + restaurant_adress + "', '" + restaurant_city + "')"; */
 
-        let query = "INSERT INTO restaurants (restaurant_name, restaurant_city, restaurant_address, restaurant_rating) VALUES ('" +
-        restaurantName + "', '" + restaurantCity + "', '" + restaurantAddress + "', '" + restaurantRating + "')";
+        let query = "INSERT INTO restaurants (restaurant_name, restaurant_city, restaurant_address, restaurant_rating)" +
+        "VALUES ('" + restaurantName + "', '" + restaurantCity + "', '" + restaurantAddress + "', '" + restaurantRating + "')";
 
         db.query(query, (err, result) => {
             if (err) {
-                res.json(err)
+                res.json(err);
             }
-            res.redirect('/restaurant')
+            res.redirect('/')
+        });
+    },
+
+    // GET EDIT RESTAURANT PAGE
+    getEditRestaurantPage: (req, res) => {
+        let restaurantId = req.params.id;
+        let query = `SELECT * FROM restaurants WHERE restaurant_id = ${restaurantId}`;
+
+        db.query(query, (err, result) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('getEditRestaurantPage', {
+                // [0] to get the input fields filled
+                restaurants: result[0]
+            });
+        });
+    },
+
+    // UPDATE RESTAURANT
+    updateRestaurant: (req, res) => {
+        let restaurantId = req.params.id;
+        let restaurantName = req.body.restaurant_name;
+        let restaurantAddress = req.body.restaurant_address;
+        let restaurantCity = req.body.restaurant_city;
+
+        let query = `UPDATE restaurants 
+        SET restaurant_name = '${restaurantName}', restaurant_address = '${restaurantAddress}', restaurant_city = '${restaurantCity}' 
+        WHERE restaurant_id = ${restaurantId}`;
+        
+        db.query(query, (err, result) => {
+            if (err) {
+                res.json(err);
+            }
+            res.redirect('/');
         })
     },
+
+    // DELETE RESTAURANT
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 16200;
 
-const { getHomePage, getAddRestaurantPage, addRestaurant } = require('./routes/restaurant');
+const { getHomePage, getAddRestaurantPage, addRestaurant, getEditRestaurantPage, updateRestaurant } = require('./routes/restaurant');
 
 // create connection to db
 const db = mysql.createConnection({
@@ -39,8 +39,12 @@ app.set('view engine', 'ejs');
 
 // routes
 app.get('/restaurant', getHomePage);
+
 app.get('/restaurant/add', getAddRestaurantPage);
 app.post('/restaurant/add', addRestaurant);
+
+app.get('/restaurant/edit/:id', getEditRestaurantPage);
+app.post('/restaurant/edit/:id', updateRestaurant);
 
 
 // listen to port
